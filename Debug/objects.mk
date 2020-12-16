a8042dee6347318409096f52129a5a71633ef94b
PiotrekOnGithub <trybalapiotr4@gmail.com>
2020-12-16 17:57:05 +0100

    OpenGL Learning 16.12.2020 17:57

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lGL -lGLU -lglfw3 -lGLEW -lXxf86vm -lpthread -lX11 -lXrandr -lm -lXi
+
